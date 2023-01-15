8b362eb39ba3ac2268cfc8aa7ab7ae5e092256b9
atias6051 <atias6051@gmail.com>
2023-01-15 09:21:08 -0500

    first commit

---
+++ backend/db/migrations/20230115063555-add-firstName-lastName-to-user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users','firstName',{
+      type: Sequelize.STRING,
+      allowNull: false,
+    },options)
+    await queryInterface.addColumn('Users','lastName',{
+      type: Sequelize.STRING,
+      allowNull: false,
+    },options)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users','firstName',options)
+    await queryInterface.removeColumn('Users','lastName',options)
+  }
+};
