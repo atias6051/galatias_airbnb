d99f9828496bd7d23d8ddd50515480cfe725ce9e
atias6051 <atias6051@gmail.com>
2023-01-25 15:55:19 -0500

    review routers good for review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -81,5 +81,31 @@ router.post('/:reviewId/images', requireAuth, checkReview, authorize,async(req,r
     res.json(newReviewImage)
 })
 
+//Edit a Review
+router.put('/:reviewId',requireAuth, checkReview, authorize, async(req,res)=>{
+    const {review,stars} = req.body
+    const validationError = {message:'Validation error',statusCode: 400,errors:{}}
+    if(!review) validationError.errors.review = "Review text is required"
+    if(!stars || isNaN(stars) || stars<1 || stars>5) validationError.errors.stars = "Stars must be an integer from 1 to 5"
+    if(validationError.errors.review || validationError.errors.stars){
+        res.statusCode = 400
+        res.json(validationError)
+    }
+    const editReview = await Review.findByPk(req.params.reviewId)
+    editReview.review = review
+    editReview.stars = stars
+    await editReview.save()
 
+    const updatedReview = await Review.findByPk(req.params.reviewId)
+    res.json(updatedReview)
+})
+
+router.delete('/:reviewId',requireAuth, checkReview, authorize, async(req,res)=>{
+    const review = await Review.findByPk(req.params.reviewId)
+    await review.destroy()
+    res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+      })
+})
 module.exports = router;
