4348149c84aa134b5a61ef1ca5cb258147c29db9
atias6051 <atias6051@gmail.com>
2023-01-25 18:21:16 -0500

    spot image router finished

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const spotImageRouter = require('./spot-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 
@@ -20,6 +21,8 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
+
+router.use('/spot-images', spotImageRouter);
 
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -4,4 +4,41 @@ const router = express.Router();
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User,Spot,SpotImage,Review, ReviewImage, sequelize, Sequelize } = require('../../db/models');
 
+const authorize = async(req,res,next) =>{
+    let spotImg = await SpotImage.findByPk(req.params.imageId,{attributes:['spotId']})
+    spotImg = spotImg.toJSON()
+    let spot = await Spot.findByPk(spotImg.spotId)
+    spot = spot.toJSON()
+    if(spot.ownerId == req.user.id) return next()
+    res.statusCode = 403
+    return res.json({
+        message: "Forbidden",
+        statusCode: 403
+    })
+}
+
+const checkSpotImg = async(req,res,next) =>{
+    const spotImg = await SpotImage.findByPk(req.params.imageId)
+    if(spotImg) return next()
+    res.statusCode = 404
+    res.json({
+        message: "Spot Image couldn't be found",
+        statusCode: 404
+    })
+}
+router.delete('/:imageId', requireAuth, checkSpotImg, authorize, async(req,res)=>{
+    const spotImg = await SpotImage.findByPk(req.params.imageId)
+    let jspotImg = spotImg.toJSON()
+    if(jspotImg.preview == true){
+        let prevSwitch = await SpotImage.findOne({where:{'preview': false}})
+        if(prevSwitch){
+            prevSwitch.preview = true
+            await prevSwitch.save()
+        }
+    }
+    await spotImg.destroy()
+
+    res.json({message:"Successfully deleted",statusCode: 200})
+})
+
 module.exports = router;
