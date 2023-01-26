e06c47f9d838b2d35074e784940822451b540640
atias6051 <atias6051@gmail.com>
2023-01-26 03:28:08 -0500

    fixed assocc

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.Spot,{foreignKey: 'spotId', onDelete: 'CASCADE'})
-      Review.belongsTo(models.User,{foreignKey: 'userId', onDelete: 'CASCADE'})
+      Review.belongsTo(models.Spot,{foreignKey: 'spotId'})
+      Review.belongsTo(models.User,{foreignKey: 'userId'})
       Review.hasMany(models.ReviewImage, {foreignKey: 'reviewId', onDelete: 'CASCADE'})
     }
   }
--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      ReviewImage.belongsTo(models.Review,{foreignKey: 'reviewId', onDelete: 'CASCADE'})
+      ReviewImage.belongsTo(models.Review,{foreignKey: 'reviewId'})
     }
   }
   ReviewImage.init({
