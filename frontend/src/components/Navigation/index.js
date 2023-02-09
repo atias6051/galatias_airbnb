ba09bff9f9c888d61c9a63eee5f268c1f88f12bb
atias6051 <atias6051@gmail.com>
2023-02-09 11:33:03 -0500

    authme4 completed

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -23,7 +24,10 @@ function Navigation({ isLoaded }){
           buttonText="Log In"
           modalComponent={<LoginFormModal />}
         />
-        <NavLink to="/signup">Sign Up</NavLink>
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
       </li>
     );
   }
