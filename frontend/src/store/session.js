e808a06d2eedc1f6116cf1b34757d103a84869f2
atias6051 <atias6051@gmail.com>
2023-02-08 20:00:57 -0500

    authMe phase 3 completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,39 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
