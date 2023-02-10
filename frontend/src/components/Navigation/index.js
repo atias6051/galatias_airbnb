9a35b0ffc8b27abaeaa2637b5cd5151ce7ec8eaa
atias6051 <atias6051@gmail.com>
2023-02-09 23:13:44 -0500

    some nav styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,7 +8,7 @@ function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul>
+    <ul className='nav-bar'>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
